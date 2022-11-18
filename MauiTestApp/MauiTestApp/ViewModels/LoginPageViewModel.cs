999fb14b70cd8827cac15990dad36d787a4c5ab5
Roger Jans <roger.jans@combitech.se>
2022-11-18 16:38:41 +0100

    MAUI Test app which can be used to reveal that showing of the text value in an Entry MAUI control is not handled correctly considering its IsPassword property value.

---
+++ MauiTestApp/MauiTestApp/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiTestApp.ViewModels
+{
+    public partial class LoginPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+        private string password = string.Empty;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(LoginCommand))]
+        private string username = string.Empty;
+
+        private bool CanLogin()
+        {
+            return Username.Length > 0 && Password.Length > 0;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanLogin), IncludeCancelCommand = true)]
+        private Task LoginAsync(CancellationToken cancelToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
